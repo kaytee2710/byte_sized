73b12557ed94ade51cb4deb6844dd59222e13b13
Ava Kieran <ava.kieran@outlook.com>
2024-10-05 10:36:52 -0400

    Adds conversion to grayscale and to ASCII

--- byte_sized.c
+++ byte_sized.c
@@ -2,9 +2,6 @@
 #include <stdlib.h>
 // png library
 
-
-
-
 /*
 STEPS TO GO THROUGH:
 - read the png file
@@ -19,24 +16,57 @@ STEPS TO GO THROUGH:
 TO-DO:
 - convertToGrayscale(image, width, height)
 - convertImageToASCII(image, width, height)
-//HIIIII
+
 
 */
 
 const char *asciiChars = "@%#*+=-:. "; // Define the ASCII characters we're using for the image
 
+// Reads in the png file
+unsigned char* readPngFile(char* fileName, int *width, int *height) {
+    FILE *fp = fopen(fileName, "rb"); // Create file pointer to read png file, open in binary mode
+
+
+    if(fp == NULL) {
+        printf("Error opening file.\n"); // Print file error
+        return -1;
+    } 
+
+}
+
 // Converts the .png image to greyscale to simplify the conversion process
 void convertToGrayscale(unsigned char *image, int width, int height) {
-    printf("Hello world\n");
+    for(int y = 0; y < height; y++) {
+        for(int x = 0; x < width; x++) {
+            int index = (y * width * x) * 4; // Move through the pixels
+            unsigned char red = image[index]; // Red values
+            unsigned char green = image[index + 1]; // Green values
+            unsigned char blue = image[index + 2]; // Blue values
 
+            // Average the RBG values to get the greyscale value 
+            unsigned char greyscaleValue = (red + green + blue) / 3; 
 
+            // Assign the initial RGB values to the new greyscale values
+            image[index] = greyscaleValue;
+            image[index + 1] = greyscaleValue;
+            image[index + 2] = greyscaleValue;
+
+        }
+    }
 }
 
 // Convert the now grayscale image to ASCII characters
 void convertImageToASCII(unsigned char *image, int width, int height) {
+    for(int y = 0; y < height; y++) {
+        for(int x = 0; x < width; x++) {
+            int index = (y * width * x) * 4; // Get the pixel value
+            unsigned char greyscaleValue = image[index]; // Greyscale value
 
-
-
+            int asciiIndex = (greyscaleValue * 9) / 255; // Convert greyscale (0 - 255) to ASCII index (0 - 9)
+            printf("%c", asciiChars[asciiIndex]); // Print the corresponding ASCII characters
+        }
+         printf("\n");
+    }
 }
 
 
@@ -44,7 +74,8 @@ void convertImageToASCII(unsigned char *image, int width, int height) {
 // Load in the image from command line and call the necessary functions
 int main(int argc, char *argv[]) {
 
-    unsigned char *image = argv[2]; // Set the second command line argument to the image we're converting
+    int width, height;
+    unsigned char *image = readPngFile(argv[1], &width, &height); // Set the second command line argument to the image we're converting
 
 
 
@@ -64,3 +95,4 @@ int main(int argc, char *argv[]) {
 
 
     return 0;
+}
